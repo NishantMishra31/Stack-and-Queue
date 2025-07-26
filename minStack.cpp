59afc1100d3efeae6f9038bb5db2c9db982fb5ef
Nishant Mishra <nishantmishra3108@gmail.com>
2025-07-26 20:43:39 +0530

    Added Files and code changes

--- minStack.cpp
+++ minStack.cpp
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class MinStack {
+class MinStack{
     
     stack <pair<int, int>> stk;
 
     public:
-        MinStack() {}
-        
-        void push(int val) {
-            if(stk.empty()) stk.push({val, val});
-            else stk.push({val, min(stk.top().second, val)});
-        }
-        
-        void pop() {
-            stk.pop();
-        }
-        
-        int top() {
-            return stk.top().first;
-        }
-        
-        int getMin() {
-            return stk.top().second;
-        }
+    MinStack() {}
+
+    void push(int val){
+        if(stk.empty()) stk.push({val, val});
+        else stk.push({val, min(stk.top().second, val)});
+    }
+
+    void pop(){
+        stk.pop();
+    }
+
+    int top(){
+        return stk.top().first;
+    }
+
+    int getMin(){
+        return stk.top().second;
+    }
 };
 
 int main(){
