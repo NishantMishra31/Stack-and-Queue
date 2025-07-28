4cdccc64f1f533e5c872a08b289d35c12fc8f091
Nishant Mishra <nishantmishra3108@gmail.com>
2025-07-28 17:38:57 +0530

    Added Files and code changes

--- next_greater_element2.cpp
+++ next_greater_element2.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector <int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+vector <int> nextGreaterElement(vector<int>& arr1, vector<int>& arr2) {
     unordered_map<int, int> nge;
     stack<int> stk;
 
-    for (int i = nums2.size() - 1; i >= 0; --i) {
-        while (!stk.empty() && stk.top() <= nums2[i]) stk.pop();
-        nge[nums2[i]] = stk.empty() ? -1 : stk.top();
-        stk.push(nums2[i]);
+    for (int i = arr2.size() - 1; i >= 0; --i) {
+        while (!stk.empty() && stk.top() <= arr2[i]) stk.pop();
+        nge[arr2[i]] = stk.empty() ? -1 : stk.top();
+        stk.push(arr2[i]);
     }
 
     vector<int> res;
-    for (int num : nums1) {
+    for (int num : arr1) {
         res.push_back(nge[num]);
     }
     return res;
@@ -21,10 +21,10 @@ vector <int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 int main() {
     int n1, n2;
     cin >> n1 >> n2;
-    vector<int> nums1(n1), nums2(n2);
-    for (int i = 0; i < n1; i++) cin >> nums1[i];
-    for (int i = 0; i < n2; i++) cin >> nums2[i];
-    vector<int> result = nextGreaterElement(nums1, nums2);
+    vector<int> arr1(n1), arr2(n2);
+    for (int i = 0; i < n1; i++) cin >> arr1[i];
+    for (int i = 0; i < n2; i++) cin >> arr2[i];
+    vector<int> result = nextGreaterElement(arr1, arr2);
     for (int x : result) cout << x << " ";
     cout << endl;
     return 0;
