ab1d3d5718f92e2db34eabfcd6a9597655dd97ce
Nishant Mishra <nishantmishra3108@gmail.com>
2025-07-30 18:28:36 +0530

    Added Files and code changes

---
+++ sum_of_subarray_ranges.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long subArrayRanges(vector <int> &arr){
+    int n = arr.size();
+    long long sum = 0;
+    for(int i = 0; i < n; i++){
+        int largest = arr[i], smallest = arr[i];
+        for(int j = i + 1; j < n; j++){
+            largest = max(largest, arr[j]);
+            smallest = min(smallest, arr[j]);
+            sum += (largest - smallest);
+        }
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    // call
+    cout << subArrayRanges(arr);
+    return 0;
+}
